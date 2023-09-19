49688f927d5b15a23440d9151a9e6e1fc770a5c3
tomer cohen <tomer66331@gmail.com>
2023-09-20 01:26:09 +0300

    start login style

--- client/app/(modal)/menu.tsx
+++ client/app/(modal)/menu.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const menu = () => {
   return (
     <View>
       <Text>menu</Text>
+      <Text>Logout</Text>
+      <TouchableOpacity>
+        <Link href="/login">Login</Link>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default menu
+export default menu;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- client/app/login.tsx
+++ client/app/login.tsx
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
 
 const login = () => {
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      
+      <Text style={styles.title}>Location Chat</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        placeholderTextColor="#777"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#777"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.forgotPassword}>Forgot Password?</Text>
     </View>
-  )
-}
+  );
+};
 
-export default login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#777',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+  loginButton: {
+    backgroundColor: '#007AFF', // Your theme color
+    borderRadius: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  forgotPassword: {
+    color: '#007AFF', // Your theme color
+    fontSize: 16,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default login;
