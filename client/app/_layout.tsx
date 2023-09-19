ccd0dcdf2b78587a373f013c6f1ed3d846afb2f3
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-09-19 16:49:52 +0200

    first commit

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,27 +1,29 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
     return (
         <Stack>
             <Stack.Screen
-                name="(tabs)"
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen name="(modal)/menu" options={{
-              presentation: 'modal',
-              title: 'Menu',
-              
-            }}/>
-            <Stack.Screen
-                name="login"
+                name='(tabs)'
                 options={{
                     headerShown: false,
                 }}
             />
             <Stack.Screen
-                name="register"
+                name='(modal)/menu'
+                options={{
+                    presentation: 'modal',
+                    title: 'Menu',
+                }}
+            />
+            <Stack.Screen
+                name='login'
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name='register'
                 options={{
                     headerShown: false,
                 }}
---
+++ client/app/profile.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const user = {
+    name: '🌟 Alice Johnson 🌟',
+    email: 'alice@example.com',
+    profilePicture:
+        'https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.webp',
+    icons: '📚 Bookworm | ☕ Coffee Lover | ✈️ Adventure Seeker',
+    bio: ' Hi there! I am Alice, a passionate bookworm who spends her days lost in the pages of novels from all around the world. When I am not curled up with a good book and a cup of coffee, you can find me planning my next adventure to far-off places. I believe in living life to the fullest and embracing every opportunity that comes my way. Let us explore the world together, one chapter at a time! 🌍📖☕',
+};
+
+const Profile = () => {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{ uri: user.profilePicture }}
+                style={styles.profilePicture}
+            />
+            <Text style={styles.name}>{user.name}</Text>
+            <Text style={styles.email}>{user.email}</Text>
+            <Text style={styles.email}>{user.icons}</Text>
+            <Text style={styles.bio}>{user.bio}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        gap: 10,
+        padding: 20,
+        // justifyContent: 'space-around',
+    },
+    profilePicture: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        marginBottom: 20,
+    },
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    email: {
+        fontSize: 16,
+    },
+    bio: {
+        fontSize: 18,
+        textAlign: 'center',
+        paddingHorizontal: 20,
+    },
+});
+
+export default Profile;
