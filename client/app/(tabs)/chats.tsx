3124a31dc8eae587d885eed8a7be092a4e17a572
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-09-24 13:58:47 +0200

    first commit - chat page created

--- client/app/(tabs)/chats.tsx
+++ client/app/(tabs)/chats.tsx
@@ -1,19 +1,96 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    FlatList,
+    StyleSheet,
+    StatusBar,
+} from 'react-native';
 
-const chats = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <Text>chats</Text>
-    </SafeAreaView>
-  )
-}
+const ChatComponent = () => {
+    const [messages, setMessages] = useState([{ text: '', user: '' }]);
+    const [newMessage, setNewMessage] = useState('');
 
-export default chats
+    const handleSendMessage = () => {
+        if (newMessage.trim() !== '') {
+            const updatedMessages = [
+                ...messages,
+                { text: newMessage, user: 'You' },
+            ];
+            setMessages(updatedMessages);
+            setNewMessage('');
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={messages}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.messageContainer}>
+                        <Text style={styles.messageText}>
+                            {item.user}: {item.text}
+                        </Text>
+                    </View>
+                )}
+            />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Type a message...'
+                    value={newMessage}
+                    onChangeText={(text) => setNewMessage(text)}
+                />
+                <TouchableOpacity
+                    style={styles.sendButton}
+                    onPress={handleSendMessage}
+                >
+                    <Text style={styles.sendButtonText}>Send</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex:1,
-        paddingTop:StatusBar.currentHeight?StatusBar.currentHeight+20:0,
+    container: {
+        flex: 1,
+        padding: 16,
+        marginTop: StatusBar.currentHeight ? StatusBar.currentHeight + 36 : 0,
     },
-})
+    messageContainer: {
+        marginBottom: 8,
+    },
+    messageText: {
+        fontSize: 16,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderColor: '#ccc',
+        paddingVertical: 8,
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 8,
+        fontSize: 16,
+    },
+    sendButton: {
+        backgroundColor: 'blue',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 4,
+        marginLeft: 8,
+    },
+    sendButtonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default ChatComponent;
