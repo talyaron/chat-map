db60b584c1adfd0540004f4e1398cb6fbcf411d6
itayz1e <itayz1e@gmail.com>
2023-09-25 21:58:20 +0300

    Designing the ABOUT page and inserting hard-coded users

--- client/app/about.tsx
+++ client/app/about.tsx
@@ -7,152 +7,191 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
+  ImageBackground,
 } from "react-native";
 
-const about = () => {
-  const [theProjectManager, setTheProjectManager] = useState(false);
-  const [talYaron, settalYaron] = useState(false);
-  const [staff, setStaff] = useState(false);
-  const [itayAmosi, setItayAmosi] = useState(false);
-  const [Nikita, setNikita] = useState(false);
-  const [itaiGelberg, setItaiGelberg] = useState(false);
+interface User {
+  id: string;
+  name: string;
+  image: string;
+  about: string;
+  linkedin: string;
+}
+interface ProjectManager {
+  id: string;
+  name: string;
+  image: string;
+  about: string;
+  linkedin: string;
+  
+}
+
+const managers: ProjectManager[] = [
+  {
+    name: "Tal yaron",
+    about:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. quisquam possimus quia rerum Similique enim harum unde sed eius iusto corrupti nihil exercitationem saepe accusamus atque neque voluptate voluptates sapiente",
+    id: "1",
+    image:
+      "https://trello-members.s3.amazonaws.com/4f97cdcfb3ef37c1662603aa/f8eebb0794e34f039a98f6d2de6b64a9/original.png",
+      linkedin: 'linkedin: linkedin/sadasdasd',
+  },
+];
+
+const users: User[] = [
+  {
+    name: "Itay Amosi",
+    id: "2",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nulla asperiores ad minus sequi amet quaerat ut sit vitae enim accusantium delectus, incidunt corporis magni libero facilis Consectetur, dolorem ea.",
+    image:
+      "https://us.123rf.com/450wm/deagreez/deagreez2104/deagreez210403136/167586649-portrait-photo-d-une-fille-souriante-heureuse-avec-une-coiffure-bob-pointant-vers-l-espace-vide-avec.jpg?ver=6",
+      linkedin: 'linkedin: linkedin/sadasdasd',
+  },
+  {
+    name: "Nikita",
+    id: "3",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellat velit alias sit earum porro modi eligendi nequeaccusantium tenetur ex rerum architecto eum cumque, explicaboofficia nihil dicta nostrum amet.",
+    image:
+      "https://trello-members.s3.amazonaws.com/6371094f58725900f3e6af44/933ac1c42df2c07ec3ad259ded59b970/original.png",
+      linkedin: 'linkedin:  linkedin/sadasgdhfdjygfugh',
+  },
+  {
+    name: "Itai Gelberg",
+    id: "4",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat velit alias sit earum porro modi eligendi neque accusantium tenetur ex rerum architecto eum cumque, explicabo officia nihil dicta nostrum amet.",
+    image:
+      "https://www.picturecorrect.com/wp-content/uploads/2013/03/better-people-photos.jpg",
+      linkedin: 'linkedin:  linkedin/dvbkgzydsKAHLDjOSDFL',
+  },
+  {
+    name: "Shlomi azn",
+    id: "5",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat velit alias sit earum porro modi eligendi neque accusantium tenetur ex rerum architecto eum cumque, explicabo officia nihil dicta nostrum amet.",
+    image:
+      "https://us.123rf.com/450wm/deagreez/deagreez2107/deagreez210704757/171784644-portrait-d-une-jolie-fille-gaie-montrant-une-offre-d-espace-de-copie-comme-suivre-l-abonnement-isol%C3%A9.jpg?ver=6",
+      linkedin: 'linkedin:  linkedin/GFDYSUHliasfhskusasid',
+  },
+  {
+    name: "Vladi",
+    id: "6",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat velit alias sit earum porro modi eligendi neque accusantium tenetur ex rerum architecto eum cumque, explicabo officia nihil dicta nostrum amet.",
+    image:
+      "https://thumbs.dreamstime.com/b/close-up-happy-teenage-girl-1465128.jpg",
+      linkedin: 'linkedin:  linkedin/sadasdasasdfagsdjhadvjsd',
+  },
+  {
+    name: "Kseniya",
+    id: "7",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat velit alias sit earum porro modi eligendi neque accusantium tenetur ex rerum architecto eum cumque, explicabo officia nihil dicta nostrum amet.",
+    image:
+      "https://www.shutterstock.com/shutterstock/photos/623101280/display_1500/stock-photo-portrait-of-serious-business-woman-standing-outside-building-623101280.jpg",
+    linkedin: 'linkedin: linkedin/sadasdhayGHEOQDWJLNRLIWEEasd',
+  },
+  {
+    name: "Tomer cohen",
+    id: "8",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat velit alias sit earum porro modi eligendi neque accusantium tenetur ex rerum architecto eum cumque, explicabo officia nihil dicta nostrum amet.",
+    image:
+      "https://media.gettyimages.com/id/571751597/photo/portrait-of-young-business-woman-holding-paper-work-looking-away-side-view.jpg?s=612x612&w=gi&k=20&c=oMhPP2YzYQVx42_gCMGPm1R_xZE5yWLlqcSl7diBYIM=",
+      linkedin: 'linkedin:  linkedin/sadasJASDGIWAHRFKUQWHLQoilasdaidasd',
+  },
+];
+
+const imageBackground = {
+  uri: "https://www.teahub.io/photos/full/1-16896_3d-world-map-hd-wallpaper-best-of-map.jpg",
+};
+const About = () => {
+  const [expandedUser, setExpandedUser] = useState<string | null>(null);
+  const [expandedManager, setExpandedManager] = useState<string | null>(null);
+
+  const toggleUser = (userId: string) => {
+    if (expandedUser === userId) {
+      setExpandedUser(null);
+    } else {
+      setExpandedUser(userId);
+    }
+  };
+
+  const toggleManager = (managerId: string) => {
+    if (expandedManager === managerId) {
+      setExpandedManager(null);
+    } else {
+      setExpandedManager(managerId);
+    }
+  };
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.listItem}>
-        <View style={styles.checkboxContainer}>
-          <Switch
-            value={theProjectManager}
-            onValueChange={(value) => setTheProjectManager(value)}
-          />
-          <Text>The project manager</Text>
-        </View>
-
-        {theProjectManager && (
-          <View style={styles.subList}>
-            <View style={styles.checkboxContainer}>
-              <Switch
-                value={talYaron}
-                onValueChange={(value) => settalYaron(value)}
-              />
-              <TouchableOpacity>
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80",
-                  }}
+    <View style={styles.box}>
+      <ImageBackground source={imageBackground} style={styles.imageBackground}>
+        <ScrollView style={styles.container}>
+      <Text style={{fontSize:50,  color: "white"}}>About as:</Text>
+          {managers.map((manager) => (
+            <View key={manager.id} style={styles.listItem}>
+              <View style={styles.checkboxContainer}>
+                <Switch
+                  value={expandedManager === manager.id}
+                  onValueChange={() => toggleManager(manager.id)}
                 />
-                <Text>Tal Yaron</Text>
-              </TouchableOpacity>
-            </View>
-
-            {talYaron && (
-              <View style={styles.subList}>
-                <Text>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
-                  quisquam possimus quia rerum. Similique enim harum unde sed
-                  eius, iusto corrupti nihil exercitationem saepe accusamus
-                  atque neque voluptate voluptates sapiente.
-                </Text>
+                <Image style={styles.image} source={{ uri: manager.image }} />
+                <Text style={styles.text}>{manager.name}</Text>
               </View>
-            )}
+
+              {expandedManager === manager.id && (
+                <View style={styles.subList}>
+                  <Text style={styles.text}>{manager.about}</Text>
+                  <Text style={{ color: "green" }}>{manager.linkedin}</Text>
+                </View>
+              )}
+            </View>
+          ))}
+
+          {users.map((user) => (
+            <View key={user.id} style={styles.listItem}>
+              <View style={styles.checkboxContainer}>
+                <Switch
+                  value={expandedUser === user.id}
+                  onValueChange={() => toggleUser(user.id)}
+                />
+                <Image style={styles.image} source={{ uri: user.image }} />
+                <Text style={styles.text}>{user.name}</Text>
+              </View>
+
+              {expandedUser === user.id && (
+                <View style={styles.subList}>
+                  <Text style={styles.text}>{user.about}</Text>
+                  <Text style={{ color: "green" }}>{user.linkedin}</Text>
+                </View>
+              )}
+            </View>
+          ))}
+          <View>
+            <Text style={{ color: "red" }}>about Team:</Text>
+            <Text style={styles.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              iusto aliquid error corporis natus! Magni odit accusamus molestiae
+              minima quae labore quam blanditiis, similique recusandae, ratione
+              officia hic. Nisi, minima.
+            </Text>
+          <Text style={{ color: "green" }}>GitHub: https://github.com/talyaron/chat-map</Text>
           </View>
-        )}
-
-        <View style={styles.checkboxContainer}>
-          <Switch value={staff} onValueChange={(value) => setStaff(value)} />
-          <Text>Oct 22 staff</Text>
-        </View>
-
-        {staff && (
-          <View style={styles.subList}>
-            <View style={styles.checkboxContainer}>
-              <Switch
-                value={itayAmosi}
-                onValueChange={(value) => setItayAmosi(value)}
-              />
-              <TouchableOpacity>
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: "https://us.123rf.com/450wm/deagreez/deagreez2104/deagreez210403136/167586649-portrait-photo-d-une-fille-souriante-heureuse-avec-une-coiffure-bob-pointant-vers-l-espace-vide-avec.jpg?ver=6",
-                  }}
-                />
-                <Text>Itay Amosi</Text>
-              </TouchableOpacity>
-            </View>
-
-            {itayAmosi && (
-              <View style={styles.subList}>
-                <Text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-                  nulla asperiores ad minus sequi amet quaerat ut sit vitae enim
-                  accusantium delectus, incidunt corporis magni libero facilis!
-                  Consectetur, dolorem ea.
-                </Text>
-              </View>
-            )}
-
-            <View style={styles.checkboxContainer}>
-              <Switch
-                value={Nikita}
-                onValueChange={(value) => setNikita(value)}
-              />
-              <TouchableOpacity>
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: "https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
-                  }}
-                />
-                <Text>Nikita</Text>
-              </TouchableOpacity>
-            </View>
-
-            {Nikita && (
-              <View style={styles.subList}>
-                <Text>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Repellat velit alias sit earum porro modi eligendi neque
-                  accusantium tenetur ex rerum architecto eum cumque, explicabo
-                  officia nihil dicta nostrum amet.
-                </Text>
-              </View>
-            )}
-            <View style={styles.checkboxContainer}>
-              <Switch
-                value={itaiGelberg}
-                onValueChange={(value) => setItaiGelberg(value)}
-              />
-              <TouchableOpacity>
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: "https://us.123rf.com/450wm/deagreez/deagreez2107/deagreez210704757/171784644-portrait-d-une-jolie-fille-gaie-montrant-une-offre-d-espace-de-copie-comme-suivre-l-abonnement-isol%C3%A9.jpg?ver=6",
-                  }}
-                />
-                <Text>Itai Gelberg</Text>
-              </TouchableOpacity>
-            </View>
-
-            {itaiGelberg && (
-              <View style={styles.subList}>
-                <Text>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Repellat velit alias sit earum porro modi eligendi neque
-                  accusantium tenetur ex rerum architecto eum cumque, explicabo
-                  officia nihil dicta nostrum amet.
-                </Text>
-              </View>
-            )}
-          </View>
-        )}
-      </View>
-    </ScrollView>
+        </ScrollView>
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   container: {
     padding: 50,
     fontSize: 16,
@@ -175,6 +214,14 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text:{
+    color: "white",
+  },
 });
 
-export default about;
+export default About;
